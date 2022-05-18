79991765729d438f21771ebda1c387d7e9b47c6e
David Fernandez Gonzalez <david.fernandezgonzalez@canonical.com>
2022-05-18 12:09:00 +0200

    good

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cve_2021_3572',
+    version='1.0',
+    description='Test CVE-2021-3572 in old pip versions',
+    url='https://github.com/litios/cve_2021_3572-old-pip',
+    author='David Fernandez',
+    author_email='davidfg168@gmail.com',
+    license='MIT',
+    py_modules=['cve_2021_3572'],
+    zip_safe=False
+)
